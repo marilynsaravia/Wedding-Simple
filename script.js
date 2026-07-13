67c96384e89beb8ae8f5981b55eaa64e65682c2f
marilynsaravia <goitiasaraviamarilyn@gmail.com>
2026-07-13 21:06:12 +0200

    feat: Add new files and structure

---
+++ script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'wedding-bg': '#F5F2EE', 
+        'wedding-dark': '#2F362B',
+        'wedding-text': '#2F362B', 
+      }, 
+      fontFamily: {
+        'serif': ['"Playfair Display"', 'serif'],
+        'sans': ['"Montserrat"', 'sans-serif'], 
+      }
+    }
+  }
+}
